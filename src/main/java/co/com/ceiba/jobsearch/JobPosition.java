8a63789f02cb75d71ab34df8cd35c0753c114b19
Emanuel Martínez <emanuel.martinez@ceiba.com.co>
2022-12-06 13:02:43 -0500

    Agrega el DTO para JobPosition

--- src/main/java/co/com/ceiba/jobsearch/JobPosition.java
+++ src/main/java/co/com/ceiba/jobsearch/JobPosition.java
@@ -1,4 +1,31 @@
 package co.com.ceiba.jobsearch;
 
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+
+@Data
 public class JobPosition {
+
+    private String id;
+
+    private String type;
+
+    private String url;
+
+    @SerializedName("created_at")
+    private String createAt;
+
+    private String company;
+
+    @SerializedName("company_url")
+    private String companyUrl;
+
+    private String title;
+
+    private String location;
+
+    private String description;
+
+    @SerializedName("company_logo")
+    private String companyLogo;
 }
