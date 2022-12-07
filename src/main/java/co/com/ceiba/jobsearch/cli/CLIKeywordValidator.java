120bbd6a83f59859b8ea787ba17b29c0490b73b4
Emanuel Martínez <emanuel.martinez@ceiba.com.co>
2022-12-07 11:07:48 -0500

    Agrega la implementación de IParameterValidator en CLIKeywordValidator

--- src/main/java/co/com/ceiba/jobsearch/cli/CLIKeywordValidator.java
+++ src/main/java/co/com/ceiba/jobsearch/cli/CLIKeywordValidator.java
@@ -1,4 +1,14 @@
 package co.com.ceiba.jobsearch.cli;
 
-public class CLIKeywordValidator {
+import com.beust.jcommander.IParameterValidator;
+import com.beust.jcommander.ParameterException;
+
+public class CLIKeywordValidator implements IParameterValidator {
+    @Override
+    public void validate(String name, String value) throws ParameterException {
+        if(!value.matches("^[a-zA-Z]+[0-9]*$")) {
+            System.err.println("Search criteria is invalid");
+            throw new ParameterException("Only letters and numbers");
+        }
+    }
 }
