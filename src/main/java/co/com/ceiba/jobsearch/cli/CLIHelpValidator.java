3a33c077af2c1bd19fc730143e69b1924bfac264
Emanuel Martínez <emanuel.martinez@ceiba.com.co>
2022-12-07 11:04:22 -0500

    Implementa la interfaz IParameterValidator en CLIHelpValidator y sobreescribe el método validate

--- src/main/java/co/com/ceiba/jobsearch/cli/CLIHelpValidator.java
+++ src/main/java/co/com/ceiba/jobsearch/cli/CLIHelpValidator.java
@@ -1,4 +1,14 @@
 package co.com.ceiba.jobsearch.cli;
 
-public class CLIHelpValidator {
+import com.beust.jcommander.IParameterValidator;
+import com.beust.jcommander.ParameterException;
+
+public class CLIHelpValidator implements IParameterValidator {
+    @Override
+    public void validate(String name, String value) throws ParameterException {
+        boolean actualValue = Boolean.parseBoolean(value);
+        if (actualValue) {
+            throw new ParameterException("Help required!");
+        }
+    }
 }
